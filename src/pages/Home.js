a68ae0afef7fec637267ab511b7303438dcf4df5
henrike32 <henrique.lambertucci@gmail.com>
2023-08-10 15:13:29 -0400

    added home page background

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,13 @@
 import React from "react"
+import "../styles/PokeHome.css";
+
 
 const Home = () => {
     return(
       <>
+      <div className="home-page">
        <h1> Home page </h1>
+      </div>
       </>
     )
 }
