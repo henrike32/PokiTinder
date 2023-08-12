bb2aef34a47961070b7039b84bed1173fa8cea8d
henrike32 <henrique.lambertucci@gmail.com>
2023-08-11 23:30:54 -0400

    cors.rb changes

--- Backend/config/initializers/cors.rb
+++ Backend/config/initializers/cors.rb
@@ -5,9 +5,11 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins 'https://poki-tinder.vercel.app'
     resource '*',
-      headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+    headers: ["Authorization"],
+    expose: ["Authorization"],
+    methods: [:get, :post, :put, :patch, :delete, :options, :head],
+    max_age: 600
   end
 end
