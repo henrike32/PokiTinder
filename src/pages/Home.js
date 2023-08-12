d899a29640146fb7b31adef4a66b196e720c577a
henrike32 <henrique.lambertucci@gmail.com>
2023-08-12 12:27:10 -0400

    more tweaks4

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,34 +1,32 @@
 import React from "react"
-import "../styles/PokeHome.css";
+import "../styles/PokeHome.css"
 import pokiHome from "../assets/pokiHome.png"
-import { Card, CardBody, CardTitle, CardSubtitle, Container } from "reactstrap";
-import { NavLink } from "react-router-dom";
-
-
+import { Card, CardBody, CardTitle, CardSubtitle, Container } from "reactstrap"
+import { NavLink } from "react-router-dom"
 
 const Home = () => {
-    return(
-      <>
-      <div className="home-page">
-     <Card className="card-container-home" >
-                <img className="card-img-home" alt="pokeHomeCard" src={pokiHome} />
-                <CardBody className="card-body-home">
-                  <CardTitle className="card-title-home" tag="h5">PokiTinder!!</CardTitle>
-                  <CardSubtitle className="card-subtitle-home" tag="h6">
-                   A place to see some cool info about Pokes!!! Feel free to browse!
-                    <br />
-                    <br />
+  return (
+    <div className="home-page">
+      <Card className="card-container-home">
+        <img className="card-img-home" alt="pokeHomeCard" src={pokiHome} />
+        <CardBody className="card-body-home">
+          <CardTitle className="card-title-home" tag="h5">
+            PokiTinder!!
+          </CardTitle>
+          <CardSubtitle className="card-subtitle-home" tag="h6">
+            A place to see some cool info about Pokes!!! Feel free to browse!
+            <br />
+            <br />
+          </CardSubtitle>
+          <div className="click-btn-container-home" tag="h6">
+            <NavLink className="click-btn-home" to={`/pokemonindex`}>
+              View Pokes
+            </NavLink>
+          </div>
+        </CardBody>
+      </Card>
+    </div>
+  );
+};
 
-                  </CardSubtitle>
-                    <div className="click-btn-container-home" tag="h6">
-                  <NavLink className="click-btn-home" to={`/pokemonindex`}>
-                      View Pokes
-                  </NavLink>                  
-                    </div>
-                </CardBody>
-              </Card>
-      </div>
-      </>
-    )
-}
 export default Home
