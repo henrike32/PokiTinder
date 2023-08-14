6a948d3a5deebbafc2b6e13b9b8b032dc2731f81
henrike32 <henrique.lambertucci@gmail.com>
2023-08-14 16:15:01 -0400

    adding the frontend url to render

--- Backend/config/initializers/cors.rb
+++ Backend/config/initializers/cors.rb
@@ -5,7 +5,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins 'fpoki-tinder.onrender.com'
     resource '*',
     headers: ["Authorization"],
     expose: ["Authorization"],
