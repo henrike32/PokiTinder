5e65c39799b69710c68c5ee42fe37e3377fdad83
henrike32 <henrique.lambertucci@gmail.com>
2023-08-14 16:02:37 -0400

    remaking some steps

--- Backend/config/environment.rb
+++ Backend/config/environment.rb
@@ -3,3 +3,7 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+  # Disable serving static files from the `/public` folder by default since
+  # Apache or NGINX already handles this.
+  config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present? || ENV['RENDER'].present?
